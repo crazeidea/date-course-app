14d686d1834807601d29e98fd608c47a27f94673
필양 <crazeidea@gmail.com>
2022-02-14 21:12:11 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Date Course App
+
+Ionic Framework와 NestJS를 이용하여 만든 데이트 코스 추천 앱 입니다.
---
+++ date-course-app-frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'date-course-app-frontend',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
