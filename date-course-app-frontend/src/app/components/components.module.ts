095614ca88ecc2e2b90b6af58ca1f2299d2c755a
필양 <crazeidea@gmail.com>
2022-02-14 23:15:01 +0900

    [Frontend] 메인 네비게이션 추가

--- date-course-app-frontend/src/app/components/components.module.ts
+++ date-course-app-frontend/src/app/components/components.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainBannerComponent } from './main-banner/main-banner.component';
 import { SwiperModule } from 'swiper/angular';
+import { MenuCardComponent } from './menu-card/menu-card.component';
+import { IonicModule } from '@ionic/angular';
 
 const components = [
-  MainBannerComponent
+  MainBannerComponent,
+  MenuCardComponent
 ];
 
 @NgModule({
   declarations: components,
   imports: [
     CommonModule,
+    IonicModule,
     SwiperModule
   ],
   exports: components
